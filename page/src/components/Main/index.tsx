5679eb7a057cb0ea5692eb2da7a55e38aaf68175
Willian Genero <willian-b-g@hotmail.com>
2020-07-18 21:10:01 -0300

    Configuração de plugins e layout mínimo

---
+++ page/src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
+
+const Main = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Willian Genero</strong>
+          <span>363 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* ProfilePage */}
+
+      <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+}
+
+export default Main;
